7c6a6912e031b5773466d6aab9e542c18925732a
MarcoLightyear <marcoemilio862@gmail.com>
2024-03-21 04:33:58 -0600

    fix: added properties argument to pump up Spark resources

--- submit.sh
+++ submit.sh
@@ -3,6 +3,7 @@ export INPUT=path-to--input-bucket
 gcloud dataproc batches submit \
     pyspark colonies.py \
     --container-image=path-to-docker-registry \
+    --properties=spark.executor.cores=8,spark.executor.memory=8g,spark.executor.memoryOverhead=2g,spark.memory.offHeap.size=2g \
     --batch=twentyeight \
     --region=us-east1 \
     --deps-bucket=path-to-jobs-bucket \
